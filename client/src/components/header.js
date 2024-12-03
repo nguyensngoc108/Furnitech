472d3aa958d4fedb025d5d3486cf82f3c7bc0c3c
VANY <91870430+ITITIU20125@users.noreply.github.com>
2024-12-03 20:05:55 +0700

    new

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,3 +1,5 @@
+// src/components/header.js
+
 const menuLinks = [
     "Home",
     "Products",
@@ -5,31 +7,65 @@ const menuLinks = [
     "About Us"
 ];
 
-const headers = () => {
+const headers = ({ className, href, onClick, children, px }) => {
     return (
-        <header className=" flex ">
-            <div className="l-container flex items-center  justify-between">
-                <a
-                    href="/"
-                    className=" space-x-0 text-3xl h-{35px}  flex  items-center gap-2.5 pr-14 "
-                >
-                    <img src="src\logo\Logo.svg" alt="Logo" width={37} height={37} />
 
-                    <span className="">Furnitech</span>
-                </a>
-                <ul className=" flex items-start justify-center px gap-10">
-                    {menuLinks.map((link) => (
-                        <li key={link}>
-                            <a href="#">{link}</a>
-                        </li>
-                    ))}
-                </ul>
+        <div className="
+         flex justify-between items-center   py-[32px] l-container 
+         ">
+            <a
+                href="/"
+                className="  
+                flex h-[34.349px] pr-[56.314px] items-center    
+            ">
+                <img src="./assets/Cir.svg" alt="Logo" width={37} height={37} />
+
+                <span className="
+                    text-black  font-DM Sans text-2xl font-extrabold tracking-wider leading-normal
+                ">
+                    Furnitech
+                </span>
+            </a>
+            <ul className=" 
+                flex items-start  px gap-10  pl-[188px] pr-[188px] 
+                text-[18px]  font-bold leading-[30px]
+                transition-colors hover:text-color-brown
+            ">
+                {menuLinks.map((link) => (
+                    <li key={link}>
+                        <a href="#">{link}</a>
+                    </li>
+                ))}
+            </ul>
+
+            <ul>
+                <li className=" 
+                    flex justify-end items-center gap-6 w-{50px} h-{50px} pr-0 fill-brown-400 bg-brown-400
+                    
+                ">
+
+                    <a
+                        href="/"
+                    >
+                        <img src="./assets/search.svg" alt="find" className="fill-brown-400 bg-brown-400" />
+                    </a>
+                    <a
+                        href="/"
+                    >
+                        <img src="./assets/person.svg" alt="person" className="fill-brown-400 bg-brown-400" />
+                    </a>
+                    <a
+                        href="/"
+                    >
+                        <img src="./assets/shopping.svg" alt="store" className="fill-brown-400 bg-brown-400" />
+                    </a>
+                </li>
+            </ul>
 
 
+        </div>
 
-            </div>
 
-        </header >
 
     )
 }
---
+++ client/src/components/section.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const section = () => {
+  return (
+    <div className='
+    rounded-[0px_40px_40px_0px] bg-white bg-opacity-24 
+    inline-flex justify-end items-start pt-[22px] pb-[22px] pl-[38.39px] pr-0 gap-[1071.61px]
+    '>
+      <h1 className='
+      text-gray-600 dark:text-gray-800 font-dm-sans text-base font-medium leading-[1.66] tracking-normal
+      '>
+        Get 20% OFF on your first order!
+      </h1>
+    </div>
+  )
+}
+
+export default section
