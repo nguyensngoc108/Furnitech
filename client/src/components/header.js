e6d3c03518807781ae3402715ef0b5e21f660c31
VANY <91870430+ITITIU20125@users.noreply.github.com>
2024-12-10 20:27:36 +0700

    header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,69 +1,88 @@
 // src/components/header.js
 
-const menuLinks = [
-    "Home",
-    "Products",
-    "Blogs",
-    "About Us"
-];
+import { useState } from "react"
+
 
 const headers = ({ className, href, onClick, children, px }) => {
+
     return (
 
-        <div className="
-         flex justify-between items-center   py-[32px] l-container 
+        <nav className="
+         w-[1920px] h-[114px] px-[350px]  justify-between  inline-flex items-center   py-[32px] l-container 
+         md:bg-top bg-center
          ">
             <a
                 href="/"
                 className="  
-                flex h-[34.349px] pr-[56.314px] items-center    
+                w-[261.31px] h-[34.35px] pr-[56.31px] justify-start items-center inline-flex   
             ">
-                <img src="./assets/Cir.svg" alt="Logo" width={37} height={37} />
 
-                <span className="
-                    text-black  font-DM Sans text-2xl font-extrabold tracking-wider leading-normal
+                <div className="self-stretch py-px justify-start items-center gap-2.5 inline-flex" >
+                    <img src="./assets/Cir.svg" alt="Logo" width={37} height={37} />
+
+                    <span className="
+                    text-black  font-DM Sans text-[32px] font-extrabold tracking-wide
                 ">
-                    Furnitech
-                </span>
+                        Furnitech
+                    </span>
+                </div>
             </a>
-            <ul className=" 
-                flex items-start  px gap-10  pl-[188px] pr-[188px] 
-                text-[18px]  font-bold leading-[30px]
-                transition-colors hover:text-color-brown
+            <ul
+                className=" 
+                h-[30px] justify-start items-start gap-10 inline-flex font-DM Sans font-normal leading-[30px]
+                transition-all duration-300 ease-out 
+                text-brown-1000
             ">
-                {menuLinks.map((link) => (
-                    <li key={link}>
-                        <a href="#">{link}</a>
-                    </li>
-                ))}
+                <li><a href="#Home" class="hover:text-brown">Home</a></li>
+                <li><a href="#Products" class="hover:text-brown" >Products</a></li>
+                <li><a href="#Blogs" class="hover:text-brown">Blogs</a></li>
+                <li><a href="#About Us" class="hover:text-brown">About Us</a></li>
             </ul>
 
             <ul>
                 <li className=" 
-                    flex justify-end items-center gap-6 w-{50px} h-{50px} pr-0 fill-brown-400 bg-brown-400
+                     h-[50px] justify-end items-center gap-6 inline-flex 
                     
+                    cursor-default md:cursor-pointer
                 ">
 
                     <a
                         href="/"
+                        className="w-[50px] h-[50px] relative "
+
                     >
-                        <img src="./assets/search.svg" alt="find" className="fill-brown-400 bg-brown-400" />
+                        <img src="./assets/search.svg" alt="find"
+                            className="
+                            w-[50px] h-[50px] rounded-full
+                            bg-[#f9f7f1]        
+                            " />
+
                     </a>
                     <a
                         href="/"
                     >
-                        <img src="./assets/person.svg" alt="person" className="fill-brown-400 bg-brown-400" />
+                        <img src="./assets/person.svg" alt="person" className="fill-brown-400" />
                     </a>
                     <a
                         href="/"
                     >
-                        <img src="./assets/shopping.svg" alt="store" className="fill-brown-400 bg-brown-400" />
+                        <img src="./assets/shopping.svg" alt="store" className="fill-brown-400" />
                     </a>
                 </li>
             </ul>
 
+            {/* Search nav */}
+            {/* <ul
+                classname={`
+                md:hidden bg-brown-400 absolute w-full h-full bottom-0 py-24 pl-4
+                duration-300 ${open ? 'left-0' : 'left-[100%]'}
+                `}
+            >
 
-        </div>
+            </ul> */}
+
+        </nav>
+
 
 
 
